60b833d011b9599462f29aa33843e1528a14e754
Anja Weigelt <aweigelt@ufl.edu>
2020-07-17 23:51:54 -0400

    Set up 404.php

--- 404.php
+++ 404.php
@@ -1,18 +1,19 @@
 <?php get_header(); ?>
 
-  <div class="container">
-    <div class="row justify-content-center">
+  <div class="container error-container">
 
+    <div class="row">
       <div class="col-md-12 error">
         <img src="<?php echo get_template_directory_uri() . '/images/404.gif' ?>" alt="Error Message" />
       </div>
+    </div>
 
+    <div class="row">
       <div class="col-md-12">
-        <h2>Page not found.</h2>
-        <p>I'm sorry, it looks like nothing was found at this location. Would you like to go back home?</p>
+        <h1>WE ARE SORRY YOU ARE LOST!</h1>
+        <p>It looks like nothing was found at this location. Would you like to go back home?</p>
         <a class="btn btn-primary btn-md" href="<?php echo home_url('/');?>">Click here</a>
       </div>
-
     </div>
   </div>
 
