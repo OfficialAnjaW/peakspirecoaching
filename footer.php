8f8daa481f1918b5c906ea9d06bf2ebcc01422a6
Anja Weigelt <aweigelt@ufl.edu>
2020-07-17 23:12:12 -0400

    Edit footer breaking points

--- footer.php
+++ footer.php
@@ -5,17 +5,17 @@
     <div class="row">
 
       <!-- Widget Area: Left Footer -->
-      <div class="col-lg-4 d-flex">
+      <div class="col-sm-4 d-flex">
         <?php dynamic_sidebar('left-footer'); ?>
       </div>
 
       <!-- Widget Area: Middle Footer -->
-      <div class="col-lg-4 d-flex">
+      <div class="col-sm-4 d-flex">
         <?php dynamic_sidebar('middle-footer'); ?>
       </div>
 
       <!-- Widget Area: Right Footer -->
-      <div class="col-lg-4 d-flex">
+      <div class="col-sm-4 d-flex">
         <?php dynamic_sidebar('right-footer'); ?>
       </div>
 
