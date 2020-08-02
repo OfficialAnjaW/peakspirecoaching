0a8161d28f174d295db8090ed2364c7dd101b9f7
Anja Weigelt <aweigelt@ufl.edu>
2020-08-02 19:22:23 -0400

    Adjust breaking points

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <?php  if(have_posts()){
           while(have_posts()){
             the_post(); ?>
-            <div class="col-md-4">
+            <div class="col-lg-4 post-overview-tiles">
 
               <div class="post-featured-image">
                 <?php the_post_thumbnail('large'); ?>
@@ -31,7 +31,7 @@
 
               <p class="post-info">Published: <a class="post-info-link" href="<?php echo get_day_link($archive_year, $archive_month, $archive_day); ?>"> <?php echo get_the_date(); ?></a></p>
 
-              <p class="post-author"><?php echo "Written by: " . get_the_author(); ?></p>
+              <p><?php echo "Written by: " . get_the_author(); ?></p>
 
               <?php the_excerpt(); ?>
 
